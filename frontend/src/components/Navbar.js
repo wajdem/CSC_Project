de7b718557ef3d85b9135a1477a7d1ba5973f8ee
wajdem <wajdi.abuarqob@gmail.com>
2023-08-26 19:10:44 +0300

    done

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+const Navbar = () => {
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>CSC</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <span>{user.username}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <button style={{marginRight:"10px "}}><Link to="/login">Login</Link></button>
+              <button><Link to="/signup">Signup</Link></button>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
+
+const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const { signup, error, isLoading } = useSignup();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Perform password match validation
+    if (password !== confPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    await signup(username, email, password);
+  };
+
+  return (
+    <form className="signup" onSubmit={handleSubmit}>
+      <h3>Sign Up</h3>
+      <label>Username:</label>
+      <input
+        type="text"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+      />
+      <label>Email:</label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <label>Password:</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <label>Confirm Password:</label>
+      <input
+        type="password"
+        onChange={(e) => setConfPassword(e.target.value)}
+        value={confPassword}
+      />
+      <button disabled={isLoading}>Sign up</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default Signup;
